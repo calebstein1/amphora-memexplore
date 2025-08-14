be72bc7db1b03552f6bc15e80a32f93b69752d5c
Caleb Stein <caleb@calebstein.net>
2025-08-14 11:02:04 -0700

    fix peek argument validation

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 int
 main(int argc, char *argv[]) {
 	int c, n;
+	char *p;
 
 	if (open_heap() != 0) return 1;
 	while ((c = getopt(argc, argv, "d:p:l")) != -1) {
@@ -25,19 +26,24 @@ main(int argc, char *argv[]) {
 				dump_block((int)strtol(optarg, NULL, 0));
 				break;
 			case 'p':
-				n = (int)(strchr(optarg, ',') - optarg);
+				p = strchr(optarg, ',');
+				if (p == NULL) goto comma_fail;
+				n = (int)(p - optarg);
 				if (n <= 0 || n >= strlen(optarg) - 1) {
-					fputs("Invalid argument for -p, requires -p <block,index>\n", stderr);
+					comma_fail:
+					(void)fputs("Invalid argument for -p, requires -p <block,index>\n", stderr);
+					close_heap();
+
 					return 1;
 				}
-				peek_addr((int)strtol(strtok(optarg, ","), NULL, 0), (int)strtol(strtok(NULL, " \n"), NULL, 0));
+				peek_addr((int)strtol(strtok(optarg, ","), NULL, 0), (int)strtol(strtok(NULL, ""), NULL, 0));
 				break;
 			case 'l':
 				list_categories();
 				break;
 			case '?':
 			default:
-				fputs("Usage: memexplore [-d <block>] [-p <block,index>] [-l]\n", stderr);
+				(void)fputs("Usage: memexplore [-d <block>] [-p <block,index>] [-l]\n", stderr);
 		}
 	}
 	close_heap();
