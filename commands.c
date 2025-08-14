b0d30ca83fdfd408ebbe058f4f7bddbbeb95e8ca
Caleb Stein <caleb@calebstein.net>
2025-08-14 09:50:09 -0700

    show printable chars in dump

--- commands.c
+++ commands.c
@@ -9,6 +9,8 @@ extern struct amphora_mem_block_metadata_t *heap_metadata;
 extern const char *category_names[];
 extern const char *cmd_list[];
 
+#define PRINTABLE_CHAR(c) ((c >= 0x20 && c <= 0x7E) ? c : '.')
+
 void
 dump_block(int blk) {
 	int i;
@@ -18,9 +20,42 @@ dump_block(int blk) {
 		return;
 	}
 	(void)printf("Memory block %d:\nCategory: %s\nAllocations: %d", blk, category_names[heap_metadata[blk].category], heap_metadata[blk].allocations);
-	for (i = 0; i < sizeof(AmphoraMemBlock); i++) {
-		if ((i & 15) == 0) (void)printf("\n%5d: ", i);
-		(void)printf("%02X ", amphora_heap[blk][i]);
+	for (i = 0; i < sizeof(AmphoraMemBlock); i += 16) {
+		(void)printf("\n%04X: %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
+			i,
+			amphora_heap[blk][i],
+			amphora_heap[blk][i + 1],
+			amphora_heap[blk][i + 2],
+			amphora_heap[blk][i + 3],
+			amphora_heap[blk][i + 4],
+			amphora_heap[blk][i + 5],
+			amphora_heap[blk][i + 6],
+			amphora_heap[blk][i + 7],
+			amphora_heap[blk][i + 8],
+			amphora_heap[blk][i + 9],
+			amphora_heap[blk][i + 10],
+			amphora_heap[blk][i + 11],
+			amphora_heap[blk][i + 12],
+			amphora_heap[blk][i + 13],
+			amphora_heap[blk][i + 14],
+			amphora_heap[blk][i + 15],
+			PRINTABLE_CHAR(amphora_heap[blk][i]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 1]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 2]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 3]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 4]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 5]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 6]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 7]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 8]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 9]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 10]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 11]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 12]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 13]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 14]),
+			PRINTABLE_CHAR(amphora_heap[blk][i + 15])
+		);
 	}
 	(void)fputs("\n", stdout);
 }
--- commands.h
+++ commands.h
@@ -3,7 +3,6 @@
 
 #include "memexplore.h"
 
-MemExploreCommand parse_cmd(const char *cmd);
 void dump_block(int);
 void peek_addr(int, int);
 void list_categories(void);
